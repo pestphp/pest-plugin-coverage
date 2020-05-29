eaf78c815f2a339fd391310ca588d90714ab6aaa
Nuno Maduro <enunomaduro@gmail.com>
2020-05-29 23:32:19 +0200

    first

--- tests/Plugin.php
+++ tests/Plugin.php
@@ -1,7 +1,37 @@
 <?php
 
-use Pest\PluginName\Plugin;
+use Pest\PluginCoverage\Coverage;
+use Pest\PluginCoverage\Plugin;
+use Pest\TestSuite;
 
-it('has plugin', function () {
-    assertTrue(class_exists(Plugin::class));
-});
+it('has plugin')->assertTrue(class_exists(Plugin::class));
+
+  it('adds coverage if --coverage exist', function () {
+      $plugin = new Plugin();
+      $testSuite = TestSuite::getInstance();
+
+      assertFalse($plugin->coverage);
+      $arguments = $plugin->handleArguments($testSuite, []);
+      assertEquals([], $arguments);
+      assertFalse($plugin->coverage);
+
+      $arguments = $plugin->handleArguments($testSuite, ['--coverage']);
+      assertEquals(['--coverage-php', Coverage::getPath()], $arguments);
+      assertTrue($plugin->coverage);
+  });
+
+  it('adds coverage if --min exist', function () {
+      $testSuite = TestSuite::getInstance();
+      $plugin = new Plugin();
+      assertEquals($plugin->coverageMin, 0.0);
+
+      assertFalse($plugin->coverage);
+      $plugin->handleArguments($testSuite, []);
+      assertEquals($plugin->coverageMin, 0.0);
+
+      $plugin->handleArguments($testSuite, ['--min=2']);
+      assertEquals($plugin->coverageMin, 2.0);
+
+      $plugin->handleArguments($testSuite, ['--min=2.4']);
+      assertEquals($plugin->coverageMin, 2.4);
+  });
